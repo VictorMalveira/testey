381ab74ee80952fa68a6ecbaba1077ff6942a9ef
victormalveira <victor_malveira@hotmail.com>
2024-07-30 15:41:33 -0300

    atualizações2 30/07

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'openui.fly.dev',
+        pathname: '/openui/**',
+      },
+    ],
+    dangerouslyAllowSVG: true, // Habilita o carregamento de SVGs remotos
+  },
 };
 
 export default nextConfig;
